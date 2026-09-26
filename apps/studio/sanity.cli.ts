790df692a97a6d6a1a9f1588b8e325414815a959
DrR0X-glitch <zach@momentech.au>
2026-09-26 21:27:43 +1000

    Add deployment appId to Sanity config

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -5,6 +5,9 @@ export default defineCliConfig({
 		projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'missing-project-id',
 		dataset: process.env.SANITY_STUDIO_DATASET || 'production'
 	},
+	deployment: {
+		appId: 'ppw60cjg1o0n33xzi6b2gfrn'
+	},
 	typegen: {
 		path: '../web/src/**/*.{ts,svelte}',
 		schema: '.sanity/schema.json',
